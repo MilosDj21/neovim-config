f1b19c88de199bebc200b33a41b1bc20f48f3d1e
Milos <m.djuka@hotmail.com>
2024-02-28 20:27:43 +0100

    comment nightfly theme

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -7,12 +7,12 @@ return {
       vim.cmd([[colorscheme tokyonight-night]])
     end,
   },
--- {
- --    "bluz71/vim-nightfly-guicolors",
- --    priority = 999, -- make sure to load this before all the other start plugins
- --    config = function()
- --      -- load the colorscheme here
- --      vim.cmd([[colorscheme nightfly]])
- --    end,
- --  }
+  -- {
+  --    "bluz71/vim-nightfly-guicolors",
+  --    priority = 999, -- make sure to load this before all the other start plugins
+  --    config = function()
+  --      -- load the colorscheme here
+  --      vim.cmd([[colorscheme nightfly]])
+  --    end,
+  --  }
 }
