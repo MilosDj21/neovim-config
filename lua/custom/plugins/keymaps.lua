e714a5b70f900df445f3f256591c6f08c82ee18f
Milos <m.djuka@hotmail.com>
2024-02-29 21:00:00 +0100

    add keymap for search replace current word

--- lua/custom/plugins/keymaps.lua
+++ lua/custom/plugins/keymaps.lua
@@ -4,4 +4,5 @@ return {
   vim.keymap.set('n', '<C-s>', ':w<CR>', { desc = 'Save document' }),
   vim.keymap.set('n', '<C-q>', ':q<CR>', { desc = 'Quit document' }),
   vim.keymap.set('n', '<C-a>', 'ggVG', { desc = 'Select all' }),
+  vim.keymap.set('n', '<C-h>', ":%s/\\<<C-r><C-w>\\>//gc", { desc = 'Search replace current word' }),
 }
