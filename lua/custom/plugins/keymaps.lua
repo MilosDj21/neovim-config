ecbab0c6ec06962af668bec38ce7af5f70ba8e40
Milos <m.djuka@hotmail.com>
2024-10-30 15:43:02 +0100

    add keymaps

--- lua/custom/plugins/keymaps.lua
+++ lua/custom/plugins/keymaps.lua
@@ -5,4 +5,8 @@ return {
   vim.keymap.set('n', '<C-q>', ':q<CR>', { desc = 'Quit document' }),
   vim.keymap.set('n', '<C-a>', 'ggVG', { desc = 'Select all' }),
   vim.keymap.set('n', '<C-h>', ":%s/\\<<C-r><C-w>\\>//gc", { desc = 'Search replace current word' }),
+  vim.keymap.set('n', '<C-d>', ":%s/\\(.\\+\\)\\.rs/\\1.rs,\\1/gc",
+    { desc = 'Add rs domains without tld to end of line' }),
+  vim.keymap.set('n', '<leader>gc', ":Git commit -m '", { desc = 'Populate command line with ":Git commit -m"' }),
+  vim.keymap.set('n', '<leader>gp', ":Git push", { desc = 'Populate command line with ":Git push"' }),
 }
