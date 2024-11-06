5d502ef0c6fa94fcaba3cff353ec659bbff4a93b
Milos <m.djuka@hotmail.com>
2024-11-06 16:03:18 +0100

    refactor: use alt instead of ctrl in tmux plugin mappings

--- lua/custom/plugins/tmux.lua
+++ lua/custom/plugins/tmux.lua
@@ -2,9 +2,9 @@ return {
   "nathom/tmux.nvim",
   config = function()
     local map = vim.api.nvim_set_keymap
-    map("n", "<C-h>", [[<cmd>lua require('tmux').move_left()<cr>]], { desc = "Go left tmux window" })
-    map("n", "<C-j>", [[<cmd>lua require('tmux').move_down()<cr>]], { desc = "Go top tmux window" })
-    map("n", "<C-k>", [[<cmd>lua require('tmux').move_up()<cr>]], { desc = "Go bottom tmux window" })
-    map("n", "<C-l>", [[<cmd>lua require('tmux').move_right()<cr>]], { desc = "Go right tmux window" })
+    map("n", "<M-h>", [[<cmd>lua require('tmux').move_left()<cr>]], { desc = "Go left tmux window" })
+    map("n", "<M-j>", [[<cmd>lua require('tmux').move_down()<cr>]], { desc = "Go top tmux window" })
+    map("n", "<M-k>", [[<cmd>lua require('tmux').move_up()<cr>]], { desc = "Go bottom tmux window" })
+    map("n", "<M-l>", [[<cmd>lua require('tmux').move_right()<cr>]], { desc = "Go right tmux window" })
   end,
 }
