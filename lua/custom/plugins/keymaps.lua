08264f2eccea89b4be9b8e7fa0cc6d0962b9acd3
Milos <m.djuka@hotmail.com>
2025-03-24 08:59:31 +0100

    Fix directory path which win explorer opens

--- lua/custom/plugins/keymaps.lua
+++ lua/custom/plugins/keymaps.lua
@@ -49,7 +49,8 @@ return {
   -- Windows related keymaps
   vim.keymap.set('n', '<leader>ol', ":!explorer.exe '$(wslpath -w $(readlink -f myLink.ln))'",
     { desc = 'Windows explorer follow symlink' }),
-  vim.keymap.set('n', '<leader>oe', "<cmd>!explorer.exe .<CR>", { desc = 'Open windows explorer in current directory' }),
+  vim.keymap.set('n', '<leader>oe', "<cmd>!explorer.exe $(wslpath -w %:p:h)<CR>",
+    { desc = 'Open windows explorer in current directory' }),
 
   -- Neorg related keymaps
   vim.keymap.set('n', '<leader>nn', "<Plug>(neorg.dirman.new-note)", { desc = 'New note' }),
